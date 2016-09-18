5b9d7ff95394dc9559c34e460a974d25028695ac
James Gibson <james@apto.com>
2016-09-18 15:30:51 -0600

    Adding hello world example

---
+++ examples/helloWorld.js
@@ -0,0 +1,19 @@
+// Adapted from https://github.com/expressjs/expressjs.com/blob/gh-pages/en/starter/hello-world.md?_ga=1.61231726.379606664.1474218809
+
+var express = require('express');
+var app = express();
+var deepthought = require('../index.js');
+
+var apiModel = deepthought.setup(app);
+
+apiModel.registerPublicRoute(
+    'get'
+    , 'Hello World Example Route'
+    , '/'
+    , function(req, res, next) { res.send('Hello World'); next();}
+    , {}
+    , 'This sample route is designed to meet all your hello world API needs.');
+
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!');
+});
