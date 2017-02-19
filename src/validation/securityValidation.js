5768f5bbd076a64b40dc090a4fc052554fbce997
Teagan Glenn <teagan@massroots.com>
2017-02-19 14:55:56 -0700

    security validations

---
+++ src/validation/securityValidation.js
@@ -0,0 +1,74 @@
+const Joi = require('joi');
+const securitySchema = require('../schema/securitySchema');
+const _ = require('underscore');
+
+// Verify result from authProvider
+const verifyAuthProviderResult = function (permissions) {
+  const result = securitySchema.validate(permissions);
+
+  return result.error
+    ? Promise.reject(result.error)
+    : Promise.resolve(result.value);
+};
+
+// Verify permissions callback
+const verifyPermissions = function (securitySchema, permissions) {
+  const errors = {};
+  var isInvalid = false;
+
+  // Iterate over required security keys
+  _.forEach(_.keys(securitySchema), function (key) {
+    const s = securitySchema[key];
+
+    if (key in permissions) {
+      if (!(s instanceof Array)) {
+        // If it's not an array - we will assume you just need the security key
+        return;
+      }
+
+      if (!s.length || _.difference(s, permissions[key]).length === 0) {
+        // We found the required permissions for this security key
+        return;
+      }
+    }
+
+    // Apparently we do not have this permission, adding
+    errors[key] = 'Missing Permission for ' + key;
+    isInvalid = true;
+  });
+
+  return isInvalid
+    ? Promise.reject(errors)
+    : Promise.resolve(true);
+};
+
+module.exports = function (authProvider, securitySchema, req, res, next) {
+  if (!securitySchema || !Object.keys(securitySchema).length) {
+    // No security defined, resolve
+    return Promise.resolve({});
+  }
+
+  // Guarantee req exists and has proper pieces of info.
+  const context = {};
+  _.forEach(['header', 'body', 'query', 'params'], function (inReq) {
+    context[inReq] = context[inReq] || {};
+  });
+
+  // Get the authentication result from the provider
+  const authResult = authProvider(req);
+
+  // Verify permissions either through promise or sync. call
+  if (authResult instanceof Promise) {
+    return authResult
+      .then(verifyAuthProviderResult)
+      .then(function (permissions) {
+        return verifyPermissions(securitySchema, permissions);
+      });
+  }
+  else {
+    return verifyAuthProviderResult(authResult)
+      .then(function (permissions) {
+        return verifyPermissions(securitySchema, permissions);
+      });
+  }
+};
---
+++ test/validation/securityValidation.test.js
@@ -0,0 +1,128 @@
+'use strict';
+
+const Chai = require('chai');
+const expect = Chai.expect;
+const securityValidation = require('../../src/validation/securityValidation');
+
+describe('#securityValidation', function () {
+  describe('invalidates', function () {
+    describe('security', function () {
+      const req = {};
+
+      it('with missing key', function (done) {
+        securityValidation(function () {
+            return {}
+          }, {
+            'api_key': []
+          }, req)
+          .then(function () {
+            done('should have been invalid');
+          })
+          .catch(function (err) {
+            expect(err)
+              .to.be.an('Object');
+
+            expect(err)
+              .to.have.property('api_key');
+
+            done();
+          })
+          .catch(done);
+      });
+
+      it('with missing permission', function (done) {
+        securityValidation(function () {
+            return {
+              'auth': [
+                'auth:write'
+              ]
+            };
+          }, {
+            'auth': [
+              'auth:write',
+              'auth:read'
+            ]
+          }, req)
+          .then(function () {
+            done('should have been invalid');
+          })
+          .catch(function (err) {
+            expect(err)
+              .to.be.an('Object');
+
+            expect(err)
+              .to.have.property('auth');
+
+            done();
+          })
+          .catch(done);
+      });
+    });
+
+    describe('authProvider', function () {
+      const req = {};
+
+      it('with incorrect resolution type', function (done) {
+        securityValidation(function () {
+            return false;
+          }, {
+            'api_key': []
+          }, req)
+          .then(function () {
+            done('should have been invalid');
+          })
+          .catch(function (err) {
+            expect(err)
+              .to.be.an('error');
+
+            done();
+          })
+          .catch(done);
+      });
+
+      it('with incorrect promise resultion type', function (done) {
+        securityValidation(function () {
+            return Promise.resolve(false);
+          }, {
+            'api_key': []
+          }, req)
+          .then(function () {
+            done('should have been invalid');
+          })
+          .catch(function (err) {
+            expect(err)
+              .to.be.an('error');
+
+            done();
+          })
+          .catch(done);
+      });
+    });
+  });
+
+  describe('validates', function () {
+    it('with non-promise resolution type', function (done) {
+      securityValidation(function () {
+          return {'api_key': []};
+        }, {
+          'api_key': []
+        }, {})
+        .then(function () {
+          done();
+        })
+        .catch(done);
+    });
+
+    it('with promise resultion type', function (done) {
+      securityValidation(function () {
+          return Promise.resolve({'api_key': []});
+        }, {
+          'api_key': []
+        }, {})
+        .then(function () {
+          done();
+        })
+        .catch(done);
+    });
+  });
+});
