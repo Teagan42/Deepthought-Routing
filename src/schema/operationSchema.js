0458709f8d9994dd1a4c248964c7452e23e7e2d0
Teagan Glenn <teagan@massroots.com>
2017-02-19 15:35:24 -0700

    Add operation swagger schema

---
+++ src/schema/operationSchema.js
@@ -0,0 +1,24 @@
+const Joi = require('joi');
+const securitySchema = require('./securitySchema');
+const parameterSchema = require('./parameterSchema');
+
+module.exports = Joi.object({
+  'tags': Joi.array()
+    .items(Joi.string().required()),
+  'summary': Joi.string()
+    .max(120)
+    .required(),
+  'description': Joi.string()
+    .required(),
+  'consumes': Joi.array()
+    .items(Joi.string().required()),
+  'produces': Joi.array()
+    .items(Joi.string().required()),
+  'parameters': Joi.array()
+    .items(parameterSchema),
+  'schemes': Joi.array()
+    .items(Joi.string()
+      .allow('http', 'https', 'ws', 'wss')),
+  'deprecated': Joi.boolean(),
+  'security': securitySchema
+});
