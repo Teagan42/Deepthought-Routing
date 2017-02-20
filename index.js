182177d31fa001516566ed3a465eae9e08725930
Teagan Glenn <teagan@massroots.com>
2017-02-20 12:09:48 -0700

    Export

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 'use strict';
 
-var apiHandler = require('./src/apiHandler');
-var apiModel = require('./src/apiModel');
-
-exports.setup = apiHandler.setup;
-exports.model = apiModel;
+exports.Router = require('./src/router');
